9f05665f400a0bfbdec9e6ae4eea3f762018c495
ItzPizzaTimez <s.hugo57540@gmail.com>
2022-08-10 21:31:05 +0200

    gf

---
+++ bassssssss.js
@@ -0,0 +1,29 @@
+const a = new Audio('AHRDAA.mp3')
+const b = new Audio('pet2.mp3')
+const pet = new Audio('pet1.mp3')
+const body = document.querySelector("body")
+const jojoimg = document.querySelector("#jojoimg")
+const shrek = document.querySelector(".ishrek")
+const text = document.querySelector("span")
+const song = [
+    a,
+    b,
+    pet
+]
+
+body.addEventListener('click', () => {
+    text.innerHTML = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+    text.classList.add("Aspan")
+    jojoimg.classList.add("imgrotate")
+    body.classList.add("bodyanimation")
+    shrek.classList.add("shrek")
+    for (audio of song) {
+        audio.play();
+    }
+})
+body.addEventListener('click', () => {
+    b.volume = 1;
+    pet.volume = 1;
+    b.play();
+    pet.play();
+})
