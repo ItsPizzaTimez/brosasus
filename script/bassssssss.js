261b4c5e1ab188c384c186b1d148e58f93209d2e
ItzPizzaTimez <s.hugo57540@gmail.com>
2022-08-12 10:24:33 +0200

    hhhhhhh

--- script/bassssssss.js
+++ script/bassssssss.js
@@ -1,6 +1,8 @@
 const a = new Audio('medias/audio/aaa.mp3')
 const b = new Audio('medias/audio/pet2.mp3')
+const aa = new Audio('medias/audio/AHRDAA.mp3')
 const pet = new Audio('medias/audio/pet1.mp3')
+const mir = new Audio('medias/audio/mir.mp3')
 const body = document.querySelector("body")
 const jojoimg = document.querySelector("#jojoimg")
 const shrek = document.querySelector(".ishrek")
@@ -8,7 +10,9 @@ const text = document.querySelector("span")
 const song = [
     a,
     b,
-    pet
+    pet,
+    mir,
+    aa
 ]
 
 body.addEventListener('click', () => {
